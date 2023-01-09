3a0eb9de42129e59ea8b797bb805a4d8f78f4fca
Ushna Ijaz <ushnaijaz99@gmail.com>
2023-01-09 22:36:42 +0500

    🐛 FIX: added gitignore

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
 
   //functions
 
+
   const addTask = () => {
     if (newTask) {
       const newTaskObj = {
@@ -38,7 +39,7 @@ function App() {
 
   const completeTask = (id) => {
     const newTask = toDo.map((task) => {
-      if(task.id === id) {
+      if (task.id === id) {
         return { ...task, status: !task.status };
       }
       return task;
@@ -54,18 +55,17 @@ function App() {
     let newEntry = {
       id: updateTask.id,
       title: e.target.value,
-      status: updateTask.status ? true : false
-    }
+      status: updateTask.status ? true : false,
+    };
     setUpdatetask(newEntry);
-  }
+  };
 
   const updatenewTask = () => {
-    let filterRecords = [...toDo].filter( task => task.id !== updateTask.id );
-    let updatedObject = [...filterRecords, updateTask]
+    let filterRecords = [...toDo].filter((task) => task.id !== updateTask.id);
+    let updatedObject = [...filterRecords, updateTask];
     setTodo(updatedObject);
-    setUpdatetask('');
-  }
-
+    setUpdatetask("");
+  };
 
   return (
     <div className="container App">
@@ -73,26 +73,26 @@ function App() {
       <h2>ToDo App</h2>
       <br></br>
 
-      
       {updateTask && updateTask ? (
         <>
           <div className="row">
             <div className="col">
-              <input 
-                value={updateTask && updateTask.title} 
-                onChange={ (e) => changenewTask(e) } 
-                className="form-control form-control-lg" 
+              <input
+                value={updateTask && updateTask.title}
+                onChange={(e) => changenewTask(e)}
+                className="form-control form-control-lg"
               />
             </div>
             <div className="col-auto">
-              <button 
-                className="btn btn-lg btn-success" 
+              <button
+                className="btn btn-lg btn-success"
                 onClick={updatenewTask}
-              >Update</button>
-              <button 
-                className="btn btn-lg btn-warning" 
-                onClick={cancelUpdate}
-              >Cancel</button>
+              >
+                Update
+              </button>
+              <button className="btn btn-lg btn-warning" onClick={cancelUpdate}>
+                Cancel
+              </button>
             </div>
           </div>
           <br />
@@ -101,59 +101,63 @@ function App() {
         <>
           <div className="row">
             <div className="col">
-              <input 
-                value={newTask} 
-                onChange={e => setNewtask(e.target.value)} 
-                className="form-control form-control-lg" 
+              <input
+                value={newTask}
+                onChange={(e) => setNewtask(e.target.value)}
+                className="form-control form-control-lg"
               />
             </div>
             <div className="col-auto">
-              <button 
-                className="btn btn-lg btn-success" 
-                onClick={addTask}
-              >Add Task</button>
+              <button className="btn btn-lg btn-success" onClick={addTask}>
+                Add Task
+              </button>
             </div>
           </div>
           <br />
         </>
       )}
 
-        {toDo && toDo.length ? "" : "No tasks available"}
+      {toDo && toDo.length ? "" : "No tasks available"}
 
-        {toDo &&
-          toDo.map((task, index) => {
-            return (
-              <React.Fragment key={task.id}>
-                <div className="col taskBg">
-                  <div className={task.status ? "done" : ""}>
-                    <span className="taskNum">{index + 1}</span>
-                    <span className="taskName">{task.title}</span>
-                  </div>
-                  <div className="icon">
-                    <span
-                      title="Make as done"
-                      onClick={() => completeTask(task.id)}
-                    >
-                      <FontAwesomeIcon icon={faSquareCheck} />
-                    </span>
-                    {task.status ? null : (
-                  <span 
-                    title="Edit"
-                    onClick={ () => setUpdatetask({ id: task.id, title: task.title, satus: task.status ? true : false }) }
-                  >
-                    <FontAwesomeIcon icon={faPen} />
-                  </span>
-                )}
-
-                    <span title="Delete" onClick={() => deleteTask(task.id)}>
-                      <FontAwesomeIcon icon={faTrashCan} />
-                    </span>
-                  </div>
+      {toDo &&
+        toDo.map((task, index) => {
+          return (
+            <React.Fragment key={task.id}>
+              <div className="col taskBg">
+                <div className={task.status ? "done" : ""}>
+                  <span className="taskNum">{index + 1}</span>
+                  <span className="taskName">{task.title}</span>
                 </div>
-                            
-        </React.Fragment>
-        );
-      })}
+                <div className="icon">
+                  <span
+                    title="Make as done"
+                    onClick={() => completeTask(task.id)}
+                  >
+                    <FontAwesomeIcon icon={faSquareCheck} />
+                  </span>
+                  {task.status ? null : (
+                    <span
+                      title="Edit"
+                      onClick={() =>
+                        setUpdatetask({
+                          id: task.id,
+                          title: task.title,
+                          satus: task.status ? true : false,
+                        })
+                      }
+                    >
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+                  )}
+
+                  <span title="Delete" onClick={() => deleteTask(task.id)}>
+                    <FontAwesomeIcon icon={faTrashCan} />
+                  </span>
+                </div>
+              </div>
+            </React.Fragment>
+          );
+        })}
     </div>
   );
 }
